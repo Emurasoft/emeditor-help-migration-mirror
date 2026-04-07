0215d9925eb7f736b9cd3102d45fd06132b45b4c
Makoto Emura <makoto@emurasoft.com>
2026-04-07 13:37:03 -0700

    fetch

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,22 @@ use worker::*;
 
 #[event(fetch)]
 async fn fetch(
-    _req: HttpRequest,
+    req: Request,
     _env: Env,
     _ctx: Context,
-) -> Result<HttpResponse> {
-    Ok(http::Response::builder()
-        .status(http::StatusCode::OK)
-        .body(Body::empty())?)
+) -> Result<Response> {
+    let url = req.url()?;
+    let new_url_str = url.as_str().replace("emeditor.org", "help.emeditor.com");
+    let new_url = Url::parse(&new_url_str)?;
+
+    let mut new_req = Request::new(new_url.as_str(), req.method())?;
+    
+    // Copy headers from the original request
+    let headers = req.headers();
+    let new_headers = new_req.headers_mut()?;
+    for (name, value) in headers {
+        new_headers.set(&name, &value)?;
+    }
+
+    Fetch::Request(new_req).send().await
 }
